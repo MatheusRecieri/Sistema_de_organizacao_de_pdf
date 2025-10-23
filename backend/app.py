5c46680f4c39a74d4606aba61c6564ee1561daac
MatheusRecieri <matheusrecieri@gmail.com>
2025-10-23 14:21:08 -0300

    Adicionando waypoint para consumo de dados do frontend

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,48 @@
-from fastapi import FastAPI # usada pra criar a web API a apartir de requisições HTTP e envia respostas em JSON
-from services.file_scanner import scan_and_split  # importa a função de scanear diretorios
+from fastapi import (
+    FastAPI,
+    HTTPException,
+)  # usada pra criar a web API a apartir de requisições HTTP e envia respostas em JSON
+from pydantic import BaseModel
+from services.file_scanner import scan_directory
+from services.run_full_pipeline import processar_pipeline
+import os
 
-app = FastAPI() # a partir disso definir endpoints(rotas)
+# from services.file_scanner import scan_and_split  # importa a função de scanear diretorios
+
+app = FastAPI()  # a partir disso definir endpoints(rotas)
+
+
+class DiretorioRequest(BaseModel):
+    caminho: str
+
 
 # definição da rota get
 @app.get("/")
 # servidor da aplicação
-def root():
+async def root():
     return {"message:" "Servidor do ORganizador contábil rodando!"}
 
-# define uma rota POST 
+
+# define uma rota POST
 # recebe um json
 @app.post("/processar")
-def processar(payload: dict):
-    caminho = payload.get("caminho") # pega o valor da chave "caminho"
-    # verificação de cainho
-    if not caminho:
-        return {"erro": "Caminho não informado."}
-    files_to_process = scan_and_split(caminho, split_pages=True)
-    return {"status": "ok", "Total": len(files_to_process), "files": files_to_process}
+def processar_diretorio(dados: DiretorioRequest):
+    caminho = dados.caminho
+
+    print(caminho)
+
+    if not os.path.exists(caminho):
+        raise HTTPException(status_code=400, detail="Caminho informado não existe")
+
+    try:
+        resultado = processar_pipeline(caminho)
+
+        return {
+            "status": "sucesso",
+            "menssagem": "Processamento conclído com sucesso",
+            "detalhes": resultado,
+        }
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Erro ao procesar arquiovs {str(e)}"
+        )
